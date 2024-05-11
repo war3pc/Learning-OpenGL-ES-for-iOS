d772abb0a3e1f0338d8519d7ff24b60a929699f4
zhangchao <hao73150811@163.com>
2024-05-11 16:30:30 +0800

    更新工程错误，最后三章的代码有点问题，还是会崩溃

---
+++ ch02_EXAMPLES/OpenGLES_Ch2_1/OpenGLES_Ch2_1/OpenGLES_Ch2_1AppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  OpenGLES_Ch2_1AppDelegate.m
+//  OpenGLES_Ch2_1
+//
+
+#import "OpenGLES_Ch2_1AppDelegate.h"
+
+@implementation OpenGLES_Ch2_1AppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationWillResignActive:(UIApplication *)application
+{
+   /*
+    Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+    Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    */
+}
+
+@end
